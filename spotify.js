5f54e2402b905f6601e56e9a1d05369506a30c78
Pratyush Kumar <pratyushgurjar07@gmail.com>
2026-07-02 14:40:02 +0530

    songbar

--- spotify.js
+++ spotify.js
@@ -1,3 +1,5 @@
+const { createElement } = require("react");
+
 function toggleLang() {
   const box = document.querySelector(".langbox");
 
@@ -8,26 +10,39 @@ function toggleLang() {
   }
 }
 
+
+ var currentcard=null;
+ var currentaudio=null;
+
+ console.log(currentaudio);
+
+
 function playSong(element) {
 
   const card = element.closest(".trend-songs") || element;
   const audio1 = card.querySelector("audio");
   const btn = card.querySelector(".playbutton");
+  const progress = document.querySelector(".progress");
+  currentcard=card;
+  currentaudio=audio1;
+  console.log(currentaudio);
 
-  
-  document.querySelectorAll("audio").forEach(a => {
-    if (a !== audio1) {
-      a.pause();
-      a.currentTime = 0;
+  songbardisplay(element);
 
-      const othercard = a.closest(".trend-songs");
-      if (othercard) {
-        const otherbtn = othercard.querySelector(".playbutton");
-        if (otherbtn) otherbtn.innerText = "▶";
+ 
+  document.querySelectorAll("audio").forEach(audio => {
+    if (audio !== audio1) {
+      audio.pause();
+      audio.currentTime = 0;
+
+      const otherCard = audio.closest(".trend-songs");
+      if (otherCard) {
+        const otherBtn = otherCard.querySelector(".playbutton");
+        if (otherBtn) otherBtn.innerText = "▶";
       }
     }
-    
   });
+
 
   if (audio1.paused) {
     audio1.play();
@@ -36,18 +51,91 @@ function playSong(element) {
     audio1.pause();
     btn.innerText = "▶";
   }
-  
-  audio1.onended = () => {
 
-    const nextcard = card.nextElementSibling;
-
-    if (nextcard && nextcard.classList.contains("trend-songs")) {
-
-      playSong(nextcard);
-
+  audio1.ontimeupdate = () => {
+    if (audio1.duration) {
+      const percent = (audio1.currentTime / audio1.duration) * 100;
+      progress.style.width = percent + "%";
     }
-};
+  };
+
+ 
+  audio1.onended = () => {
+    const nextCard = card.nextElementSibling;
+
+    if (nextCard && nextCard.classList.contains("trend-songs")) {
+      playSong(nextCard);
+    }
+  };
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+function nextsong() {
+
+  if (!currentcard) return;
+
+  const nextCard = currentcard.nextElementSibling;
+
+  if (nextCard && nextCard.classList.contains("trend-songs")) {
+    playSong(nextCard);
+  }
+}
+function prevsong() {
+  if (!currentcard) return;
+
+  const prevCard = currentcard.previousElementSibling;
+
+  if (prevCard && prevCard.classList.contains("trend-songs")) {
+    playSong(prevCard);
+  }
+}
+
+function songbar() {
+console.log(currentaudio);
+  if (!currentaudio) return;
+
+  const btn = document.querySelector(".onoff");
+
+  if (currentaudio.paused) {
+    currentaudio.play();
+    btn.innerText = "⏸";
+  } else {
+    currentaudio.pause();
+    btn.innerText = "▶";
+  }
+}
+
+
+
+
+function songbardisplay(element) {
+    let img = element.querySelector("img");
+    let songimg = document.querySelector(".songbar img");
+    document.querySelector(".songbar").style.display="flex";
+    songimg.src = img.src;
+    document.querySelector(".songbar h4").innerText=element.querySelector("h4").innerText;
+    document.querySelector(".songbar p").innerText=element.querySelector("p").innerText;
+}
+
+
+
+
+
+
+
+
+
 
 const originalcontent =
   document.querySelector(".songs-box").innerHTML;
@@ -59,18 +147,10 @@ function showallsongs() {
 function showallsongsback() {
   document.querySelector(".songs-box").innerHTML = originalcontent;
 }
-const body = document.body;
+ 
+ function playsongbar(){
+   
+    
+ }
 
-// body.addEventListener("keydown", (e) => {
-//     if(e.key.toLowerCase()==="k"){
-//       const currentaudio=[...document.querySelectorAll("audio")].find(audio=>!audio.paused);
-
-//      if(currentaudio){
-//             currentaudio.pause();
-//       }
-//       else{
-//           const audio = document.querySelector("audio");
-//           if (audio) audio.play();
-//       }
-//     }
-// });  
+ 
